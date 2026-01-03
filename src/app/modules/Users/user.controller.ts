6fa571f26da23ba0c0f38a4491ee4b56a8e45fb6
devrivzy <rizvyhq1@gmail.com>
2026-01-03 22:40:57 +0600

    feat: Add try-catch block to createAdmin function

--- src/app/modules/Users/user.controller.ts
+++ src/app/modules/Users/user.controller.ts
@@ -1,8 +1,11 @@
 import type { NextFunction, Request, Response } from "express";
+import catchAsync from "../../../shared/catchAsync";
 
-const createAdmin = async (req: Request, res: Response, next: NextFunction) => {
-  console.log(req.body);
-};
+const createAdmin = catchAsync(
+  async (req: Request, res: Response, next: NextFunction) => {
+    console.log(req.body);
+  }
+);
 
 export const userController = {
   createAdmin,
