2fe49ac63929c49ea07bd0496d968d43e367b18f
devrivzy <rizvyhq1@gmail.com>
2026-01-04 00:48:47 +0600

    feat: Add bcrypt hashing for user passwords

--- src/app/modules/Auth/auth.service.ts
+++ src/app/modules/Auth/auth.service.ts
@@ -1,12 +1,23 @@
+import bcrypt from "bcrypt";
+import { prisma } from "../../../lib/prisma";
 const register = async (payload: any) => {
-  console.log("from Servies", payload);
-const userData = payload.user;
-console.log("ONLY USERDATA : ", userData)
-console.log("password", payload.password)
+  const hashedPass: string = await bcrypt.hash(payload.password, 9);
+  console.log("hashed", payload.password, { hashedPass });
 
-co 
+  const userData = {
+    name: payload.user.name,
+    email: payload.user.email,
+    password: hashedPass,
+  };
 
+  console.log(userData);
 
+  const result = await prisma.user.create({
+    data: userData,
+  });
+  
+
+  return result;
 };
 
 const login = async (payload: any) => {
