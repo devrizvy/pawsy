ac9bd283e8147505c3b7c8f237726d90dd231e0d
devrivzy <rizvyhq1@gmail.com>
2026-01-03 20:20:54 +0600

    feat: Implement user module with routes, controller, service, and validation files

--- src/app.ts
+++ src/app.ts
@@ -6,11 +6,14 @@ import express, {
 } from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
+import router from "./app/routes";
 const app: Application = express();
 app.use(cors());
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.use("/api/v1/" , router)
 
 app.get("/", (req: Request, res: Response, next: NextFunction) => {
 	res.send({
---
+++ src/app/routes/index.ts
@@ -0,0 +1,17 @@
+import express from "express";
+import app from "../../app";
+import { userRoutes } from "../modules/Users/user.route";
+
+const router = express.Router();
+
+const moduleRoutes = [
+  {
+    path: "/user",
+    route: userRoutes,
+  },
+];
+
+// to match this syntex : router.use("/admin" , adminRoutes)
+moduleRoutes.forEach((route) => router.use(route.path, route.route));
+
+export default router;
